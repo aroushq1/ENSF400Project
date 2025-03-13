f662abfa62bcd2c9b18488f5fb5bc961377a9061
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-13 12:04:53 -0600

    add: finish server file

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const socketIO = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+
+app.use(express.static("public"));
+
+server.listen(3000, () => {
+    console.log("Server started on port 3000")
+});
