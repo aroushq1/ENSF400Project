abfc0fe770f46fe8bb61776c9af5cc65ef389d3d
Umar Khan <muhammed.khan@ucalgary.ca>
2025-03-13 13:17:32 -0600

    add: adding move functions

--- public/game.js
+++ public/game.js
@@ -26,3 +26,55 @@ const aiPaddle = {
     height: 80,
     dy: 4
 }
+
+function movePaddle(paddle, y){
+    paddle.y = y;
+
+    if(paddle.y < 0) paddle.y = 0;
+    if(paddle.y + paddle.height > canvas.height) paddle.y = canvas.height - paddle.height;
+}
+
+function moveBall(){
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    if( ball.y + ball.radius < userPaddle.x + userPaddle.width  &&
+        ball.x + ball.radius > userPaddle.x                     &&
+        ball.y - ball.radius < userPaddle.y + userPaddle.height &&
+        ball.y + ball.radius > userPaddle.y
+    ){
+        ball.dx *= -1;
+        
+        ball.speed++;
+        let collidePoint = ball.y - (userPaddle.y + userPaddle.height/2);
+        collidePoint = collidePoint / (userPaddle.height/2);
+        let angleRad = (Math.PI / 4)* collidePoint;
+        ball.dy = ball.speed * Math.sin(angleRad);
+        adjustAiTarget();
+    }
+
+    if( ball.y + ball.radius < aiPaddle.x + aiPaddle.width  &&
+        ball.x + ball.radius > aiPaddle.x                     &&
+        ball.y - ball.radius < aiPaddle.y + aiPaddle.height &&
+        ball.y + ball.radius > aiPaddle.y
+    ){
+        ball.dx *= -1;
+        
+        ball.speed++;
+        let collidePoint = ball.y - (aiPaddle.y + aiPaddle.height/2);
+        collidePoint = collidePoint / (aiPaddle.height/2);
+        let angleRad = (Math.PI / 4)* collidePoint;
+        ball.dy = ball.speed * Math.sin(angleRad);
+        adjustAiTarget();
+    }
+
+    if(ball.x + ball.radius > canvas.width){
+        scores[0]++;
+        resetBall(-1);
+    }
+
+    if(ball.x + ball.radius < 0){
+        scores[1]++;
+        resetBall(1);
+    }
+}
