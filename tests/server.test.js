9e307267b0f3b2c51e8f40ae4bf5f4dd4f016dcf
Umar Khan <muhammed.khan@ucalgary.ca>
2025-04-01 16:21:10 -0600

    add: initial server test logic

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,10 +1,39 @@
 const request = require('supertest');
-const app = require('../server');
+const { Server } = require('socket.io');
+const http = require('http');
+const express = require('express');
+
+// Create a new Express app and socket server for testing
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
 
 describe('GET /', () => {
     it('should return Hello World', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
         expect(res.text).toBe('Hello World');
+    });
+});
+
+// Socket.io connection test
+describe('Socket.io', () => {
+    it('should handle socket connections', (done) => {
+        const socket = require('socket.io-client')('http://localhost:3000');
+        
+        socket.on('connect', () => {
+            expect(socket.connected).toBe(true);
+            socket.disconnect();
+            done();
+        });
     });
 });
